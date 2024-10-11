9106af2e922cdd28fd2f6e5cd69235ae2843e578
Tandé Hugo <htande@hotmail.fr>
2024-10-11 18:36:53 +0000

    JDR TEST

--- Semaine 2/TEST Js/index.js
+++ Semaine 2/TEST Js/index.js
@@ -12,7 +12,7 @@ function getValidValue(message, min, max) {
 let joueur = {
     HP : getValidValue('Choisissez vos points de vie entre 1 et 100',1, 100),
     AP : getValidValue('Choisissez vos points d\'attaque entre 1 et 3',1, 3),
-    
+    DDG : 10,
     
 };
 
@@ -21,43 +21,49 @@ console.log('vos stats sont : ', joueur);
 let ennemi = {
     HP : Math.ceil(Math.random() * 100),
     AP : Math.ceil(Math.random()*3),
+    DDG : 10,
+    
 }
 
 console.log('Les stats Ennemi sont : ', ennemi);
-
-
-function JDR(){
-while(joueur.HP > 0 && ennemi.HP > 0){
-            let resultatAttaque = Math.ceil(Math.random() * 20 + 1);
-        if (resultatAttaque > 18) {
-            ennemi.HP = ennemi.HP - joueur.AP * 2;
-            console.log('Réussite critique \ '+'les pv de l\'ennemi sont a : \ '+ennemi.HP);
-        } else if (resultatAttaque < 5) {
-            joueur.HP = joueur.HP - 1;
-            console.log('Echec critique \ '+'les pv du joueur sont a : \ '+joueur.HP);
-        } else {
-            ennemi.HP = ennemi.HP - joueur.AP;
-            console.log('Le joueur attaque \ '+'les point de vie de l\'ennemi sont a:\ '+ennemi.HP);
-        }
-
-if (ennemi.HP > 0) {
-    resultatAttaque = Math.ceil(Math.random() * 20 + 1);
-    if (resultatAttaque > 18) {
-        joueur.HP = joueur.HP - ennemi.AP * 2;
-        console.log('Réussite critique,\ '+'les pv du Joueur sont a :\ '+joueur.HP);
-    } else if (resultatAttaque < 5) {
-        ennemi.HP = ennemi.HP - 1;
-        console.log('Echec critique,\ '+'les pv de l\'ennemi sont a :\ '+ennemi.HP);
-    } else if (resultatAttaque > 5 && resultatAttaque < 18){
-        joueur.HP = joueur.HP - ennemi.AP;
-        console.log('L\'ennemi attaque,\ '+'les point de vie du joueur sont a :\ '+joueur.HP);
-     } else if (joueur.HP <= 0){
-         return('VICTOIRE !')
-     } else {
-         return('TRY AGAIN !')
-     }
+function attack(attaquant, defenseur){
+    let resultatAttaque = Math.ceil(Math.random() * 20 + 1);
+    
+    if(resultatAttaque > 18){
+        defenseur.HP -= attaquant.AP * 2;
+        console.log('Coup critique ! ' + attaquant.nom + ' inflige ' 
+        + attaquant.AP*2 + ' \ point de dégats à ' + defenseur.nom)
+    } else if (defenseur.DDG >= Math.ceil(Math.random()*100)) {
+        console.log(defenseur.nom + ' \ esquive l\'attaque de ' + attaquant.nom)
+    } else {
+        defenseur.HP -= attaquant.AP;
+        console.log(attaquant.nom + ' \  inflige ' + attaquant.AP + ' \  points de dégats à ' + defenseur.nom)
     }
 }
+
+function JDR(){
+    joueur.nom = prompt('Entrer votre nom  ');
+    ennemi.nom = prompt('Entrer le nom de votre nemesis ');
+    
+    while(joueur.HP > 0 && ennemi.HP > 0) {
+        attack(joueur, ennemi);
+        console.log('point de vie du joueur ' + joueur.HP)
+        console.log('point de vie de l\'ennemi ' + ennemi.HP)
+        
+        if(ennemi.HP <= 0) {
+            return 'VICTOIRE !'
+        }
+        attack(ennemi, joueur);
+        console.log('point de vie du joueur ' + joueur.HP)
+        console.log('point de vie de l\'ennemi ' + ennemi.HP)
+        
+        if(joueur.HP <=0 ) {
+            return 'TRY AGAIN !'
+        }
+        
+    }
 }
+
+
 let jdr = JDR();
 console.log(jdr);
